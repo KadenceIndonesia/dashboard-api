324ec99f20bd2d0c201ac1425319def5f8ee7bc6
rizkymalm <rizkymalm@gmail.com>
2021-08-29 14:59:57 +0700

    data filter by break

--- controllers/api.js
+++ controllers/api.js
@@ -343,6 +343,158 @@ exports.getDataByBreak = async function (req, res) {
   }
 };
 
+
+exports.dataByBreak = async function (req, res) {
+  var qidx = req.params.qidx;
+  var break1 = req.body.break1;
+  var code1 = req.body.code1;
+  var break2 = req.body.break2;
+  var code2 = req.body.code2;
+  var break3 = req.body.break3;
+  var code3 = req.body.code3;
+  var pid = req.params.pid;
+  var data = await excelData(pid);
+  var getattributebypid = await getAttributesByPid(pid, qidx);
+  var rawdata = [];
+  var labelAttr;
+
+  const filterBreak1 = (i) => {
+    if(code1!="all"){
+        return data[i][break1] == code1
+    }else{
+      return " "
+    }
+  }
+
+  const filterLogic = (i) => {
+    if(code2!="all" && code3=="all"){
+        return data[i][break2] == code2
+    }else if(code2=="all" && code3!="all"){
+        return data[i][break3] == code3
+    }else if(code2!="all" && code3!="all"){
+      return data[i][break2] == code2 && data[i][break3] == code3
+    }else{
+      return " "
+    }
+  }
+
+  if (getattributebypid.length > 0) {
+    if (getattributebypid[0].type == "SA") {
+      labelAttr = getattributebypid[0].attribute;
+      for (let i = 0; i < data.length; i++) {
+        if (data[i][qidx] != -1 && filterBreak1(i) && filterLogic(i)) {
+          for (let x = 0; x < labelAttr.length; x++) {
+            if (labelAttr[x].code == data[i][qidx]) {
+              rawdata.push({
+                sbjnum: data[i]["SbjNum"],
+                code: parseInt(labelAttr[x].code),
+                label: labelAttr[x].label,
+                kota: data[i]["Kota"],
+                y: 1,
+              });
+            }
+          }
+        }
+      }
+    } else if (getattributebypid[0].type == "MA") {
+      labelAttr = getattributebypid[0].attribute;
+      for (let i = 0; i < data.length; i++) {
+        for (let y = 1; y <= getattributebypid[0].attribute.length; y++) {
+          if (
+            data[i][qidx + "_O" + y] != -1 &&
+            data[i][qidx + "_O" + y] < getattributebypid[0].attribute.length &&
+            filterBreak1(i)
+            && filterLogic(i)
+          ) {
+            for (let z = 0; z < labelAttr.length; z++) {
+              if (labelAttr[z].code == data[i][qidx + "_O" + y]) {
+                rawdata.push({
+                  sbjnum: data[i]["SbjNum"],
+                  code: parseInt(labelAttr[z].code),
+                  label: labelAttr[z].label,
+                  kota: data[i]["Kota"],
+                  y: 1,
+                });
+              }
+            }
+          }
+        }
+      }
+    } else if (getattributebypid[0].type == "LOOPSA") {
+      labelAttr = getattributebypid[0].attribute;
+      for (let i = 0; i < data.length; i++) {
+        for (let x = 0; x < getattributebypid[0].loopLabel.length; x++) {
+          if (
+            data[i][getattributebypid[0].loopLabel[x] + "_" + qidx] != -1 && filterBreak1(i)  && filterLogic(i)
+          )
+            for (let z = 0; z < labelAttr.length; z++) {
+              if (
+                labelAttr[z].code ==
+                data[i][getattributebypid[0].loopLabel[x] + "_" + qidx]
+              ) {
+                var splitCode = getattributebypid[0].loopLabel[x].split(".");
+                var parentCode = parseInt(splitCode);
+                rawdata.push({
+                  sbjnum: data[i]["SbjNum"],
+                  code: parseInt(labelAttr[z].code),
+                  label: labelAttr[z].label,
+                  parentcode: parentCode,
+                  parentlabel: getattributebypid[0].loopLabel[x],
+                  kota: data[i]["Kota"],
+                  y: 1,
+                });
+              }
+            }
+        }
+      }
+    } else if (getattributebypid[0].type == "LOOPMA") {
+      labelAttr = getattributebypid[0].attribute;
+      for (let i = 0; i < data.length; i++) {
+        for (let x = 0; x < getattributebypid[0].loopLabel.length; x++) {
+          for (let y = 1; y <= getattributebypid[0].attribute.length; y++) {
+            if (
+              data[i][
+                getattributebypid[0].loopLabel[x] + "_" + qidx + "_O" + y
+              ] != -1 &&
+              filterBreak1(i) &&
+              filterLogic(i)
+            ) {
+              for (let z = 0; z < labelAttr.length; z++) {
+                if (
+                  labelAttr[z].code ==
+                  data[i][
+                    getattributebypid[0].loopLabel[x] + "_" + qidx + "_O" + y
+                  ]
+                ) {
+                  var splitCode = getattributebypid[0].loopLabel[x].split(".");
+                  var parentCode = parseInt(splitCode);
+                  rawdata.push({
+                    sbjnum: data[i]["SbjNum"],
+                    code: parseInt(labelAttr[z].code),
+                    label: labelAttr[z].label,
+                    parentcode: parentCode,
+                    parentlabel: getattributebypid[0].loopLabel[x],
+                    kota: data[i]["Kota"],
+                    y: 1,
+                  });
+                }
+              }
+            }
+          }
+        }
+      }
+    }
+    res.send(rawdata);
+  } else {
+    res.status(404).json({
+      message: "question not found",
+    });
+  }
+};
+
+
+
+
 exports.getSliceData = async function (req, res) {
   var qidx = req.params.qidx;
   var pid = req.params.pid;
--- routes/api.js
+++ routes/api.js
@@ -10,6 +10,8 @@ Router.get("/:pid/data/:qidx/slice", apiControllers.getSliceData);
 Router.get("/:pid/data/:qidx/attribute", apiControllers.getAttributeData);
 Router.get("/:pid/data/:qidx/break/:break/:code", apiControllers.getDataByBreak);
 
+Router.post("/:pid/data/:qidx/break/", apiControllers.dataByBreak);
+
 Router.get("/:pid/topbreak/:qidx", apiControllers.getTopBreak);
 
 module.exports = Router;
