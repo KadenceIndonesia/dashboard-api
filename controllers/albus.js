4a497a30f138a39ae7c91f29c3b6e7f418d42348
rizkymalm <rizkymalm@gmail.com>
2024-01-11 01:28:24 +0700

    update albus

--- controllers/albus.js
+++ controllers/albus.js
@@ -43,3 +43,60 @@ exports.rawdataServicePoint = async function (req, res) {
     res.status(400).send(error);
   }
 };
+
+exports.rawdataBranch = async function (req, res) {
+  try {
+    const pid = req.params.pid;
+    var result = [];
+    var data = await excelDataSubDir(pid, 'branch');
+
+    for (let i = 0; i < data.length; i++) {
+      var _getBranchByRawdataTags = await getBranchByRawdataTags(
+        pid,
+        data[i]['S0'],
+        'branch'
+      );
+      result.push({
+        SbjNum: data[i].SbjNum,
+        branch: _getBranchByRawdataTags.branch,
+        city: _getBranchByRawdataTags.city,
+        date: excelDatetoJS(data[i]['TglKunjungan']),
+        agent: data[i]['NamaShopper'],
+        storage: data[i]['storage'],
+        key: data[i]['key'],
+      });
+    }
+    res.status(200).json({
+      statusCode: 200,
+      message: 'Success getrawdata branch',
+      data: result,
+    });
+  } catch (error) {
+    res.status(400).send(error);
+  }
+};
+
+exports.rawdataKYC = async function (req, res) {
+  try {
+    const pid = req.params.pid;
+    var result = [];
+    var data = await excelDataSubDir(pid, 'kyc');
+
+    for (let i = 0; i < data.length; i++) {
+      result.push({
+        SbjNum: data[i].SbjNum,
+        date: excelDatetoJS(data[i]['TglVideo']),
+        agent: data[i]['NamaShopper'],
+        storage: data[i]['storage'],
+        key: data[i]['key'],
+      });
+    }
+    res.status(200).json({
+      statusCode: 200,
+      message: 'Success getrawdata kyc',
+      data: result,
+    });
+  } catch (error) {
+    res.status(400).send(error);
+  }
+};
--- routes/albus.js
+++ routes/albus.js
@@ -4,6 +4,8 @@ const Router = express.Router();
 
 Router.get('/:pid/test', albusController.testController);
 Router.get('/:pid/rawdata/service-point', albusController.rawdataServicePoint);
+Router.get('/:pid/rawdata/branch', albusController.rawdataBranch);
+Router.get('/:pid/rawdata/kyc', albusController.rawdataKYC);
 
 
 module.exports = Router;
