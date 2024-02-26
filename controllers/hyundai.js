46be512f198b6c3ecc6323a92c41bef67b568f19
rizkymalm <rizkymalm@gmail.com>
2024-02-26 19:39:23 +0700

    Update hyundai.js

--- controllers/hyundai.js
+++ controllers/hyundai.js
@@ -1335,7 +1335,7 @@ exports.getTrendedScoreDealerAllWave = async function (req, res) {
           );
         for (let z = 0; z < _scoreTouchPointByParentDealer.length; z++) {
           datas.push(
-            _scoreTouchPointByParentDealer[z].score > 0
+            _scoreTouchPointByParentDealer[z].score > -1
               ? decimalPlaces(_scoreTouchPointByParentDealer[z].score, 2)
               : _scoreTouchPointByParentDealer[z].score
           );
